6f097dd57f568cbbc295f0bebaa45d1fe3ff7c7d
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-25 03:26:30 +0530

    initial setup

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![allow(unreachable_code)]
+
+use std::io;
+
+fn main() -> io::Result<()> {
+    let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
+
+    let mut buf = [0u8; 1504];
+    loop {
+        let nbytes = nic.recv(&mut buf[..])?;
+        let flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        eprintln!("flags {} proto {} {:?}", flags, proto, &buf[4..nbytes]);
+    }
+    Ok(())
+}
